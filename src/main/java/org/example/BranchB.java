1c46052c63fc8946a0634003900215ab726ac622
SafaBabaei <zeyneb.babaei@gmail.com>
2023-05-14 11:08:26 -0400

    branch b created

---
+++ src/main/java/org/example/BranchB.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class BranchB {
+     public void methodB(){
+         System.out.println("-------Branch B has been created----------");
+     }
+}
